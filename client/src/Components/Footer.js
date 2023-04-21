419cec7eef3602d2fe3eee2382a9a7bbb37fd7b2
badiadi~ <badari.adam@gmail.com>
2023-04-21 10:15:23 +0200

    footer done

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,17 +1,77 @@
 import React from 'react';
-import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
-export const Footer = () => {
+export function Footer() {
   return (
-    <MDBFooter className='footer' style={{ backgroundColor: '#222831' }}>
-      <MDBContainer className='p-4'></MDBContainer>
+    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#393E46', color: '#00ADB5' }}>
+      <MDBContainer className='pt-4'>
+        <section className='mb-4'>
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="sm"
+            className=' m-1'
+            href='https://www.facebook.com/adam.badari'
+            role='button'
+            noRipple='true'
+          >
+            <MDBIcon fab className='fab fa-facebook-f' />
+          </MDBBtn>
 
-      <div className='text-center p-3' style={{ backgroundColor: 'black' }}>
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="sm"
+            className=' m-1'
+            href='https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2Fadambadari%3Figshid%3DMGNiNDI5ZTU%253D%26fbclid%3DIwAR1Uj8-EIdpmtbV_kxKu1Az42G5O76ClLpxao1dOUfRoQv_-PCqR6yA2ZEk&h=AT2Dzshsw4VFmQTLW-LKEAAVYrkxshJ1mG-LBNdY0GxegKwWrsX149VifoYZcENRWWWEG0CGTWYvYBkPY-AxxllPYr0fZ_u_N7ZuKCIrVJqxsp-FAwmE0IR9Unp0U7EgO3GKOQ'
+            role='button'
+            noRipple='true'
+          >
+            <MDBIcon fab className='fa-instagram' />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="sm"
+            className=' m-1'
+            href='https://www.linkedin.com/in/%C3%A1d%C3%A1m-badari-711638263'
+            role='button'
+            noRipple='true'
+          >
+            <MDBIcon fab className='fa-linkedin' />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="sm"
+            className='m-1'
+            href='https://github.com/Recki15/crafttips'
+            role='button'
+            noRipple='true'
+          >
+            <MDBIcon fab className='fa-github' />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)', color: '#00ADB5' }}>
         © 2023 Copyright:
-        <a className='text-white' href='https://mdbootstrap.com/'>
-          Crafttips.com
-        </a>
+          Crafttips
+        
       </div>
     </MDBFooter>
-  )
+  );
 }
