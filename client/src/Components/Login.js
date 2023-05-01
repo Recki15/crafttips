6d71adc83463057a426ec3a44249afc3adebf133
Recki15 <aticsati16@gmail.com>
2023-05-01 19:10:13 +0200

    Login bugfix
    Fixed a login bug. now its 120% working.

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -45,7 +45,7 @@ export const Login = () => {
                                 <Alert severity="error">{msg}</Alert>
                                 </div>
                                 <div className="field mt-5">
-                                    <label className="label">Email or Username</label>
+                                    <label className="label">Email</label>
                                     <div className="controls">
                                         <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} />
                                     </div>
